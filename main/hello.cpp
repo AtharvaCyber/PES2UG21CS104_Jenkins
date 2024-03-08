7ae34f6edf175925eef195523e8ff6e6d0d88254
Atharva Menkudle <107703684+AtharvaCyber@users.noreply.github.com>
2024-03-08 15:14:30 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
   cout<<  "Hello Atharva_PES2UG21CS104 here";
+  cout<<  "Welcome to PESU";
   return 0;
 }
