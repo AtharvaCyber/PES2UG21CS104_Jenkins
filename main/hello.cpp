9e10f60e399b8878a61c0e7d910f17c4fb26a91b
Atharva Menkudle <a1menkudle@gmail.com>
2024-03-08 12:06:45 +0530

    Doing Commit

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout<<  "Hello Atharva_PES2UG21CS104 here";
+  return 0;
+}
